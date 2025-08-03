9e7f43836ce7119e1ed54a83851e57c23fe6ecc8
GaussianWonder <teo.virghi@yahoo.ro>
2025-08-03 08:17:18 +0000

    feat: move existing examples as templates

---
+++ templates/node-package/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * {{ description }}
+ * @param name who are you?
+ * @returns {{ name }} greet
+ */
+export function hello(name: string): string {
+  return `Hello, ${name}! This is {{ name }}.`;
+}
